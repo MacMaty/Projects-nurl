66b7011db33c1aee6d7d3243a00fe15e3267c506
Mathieu BOYER <mat.boyer95@gmail.com>
2014-05-30 10:42:42 +0200

    début projet

---
+++ Project-nurl/Project-nurl/MyClass.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mathieu
+ * Date: 30/05/2014
+ * Time: 10:39
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace Project_nurl
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		
+	}
+}
---
+++ Project-nurl/Project-nurl/Test1.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mathieu
+ * Date: 30/05/2014
+ * Time: 10:40
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NUnit.Framework;
+
+namespace Project_nurl
+{
+	 [TestFixture]   
+ public class MyTest   
+ {    
+  [Test]    
+  public void FailingTest()    
+  {     
+   Assert.Fail("This test fails.");    
+  }   
+ }  
+}
